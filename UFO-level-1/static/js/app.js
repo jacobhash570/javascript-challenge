7d47932967db08ed5ce95df3977402db82ff685e
Jacob <jacob.hash570@gmail.com>
2021-09-28 18:12:17 -0400

    Data table added.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,3 +5,13 @@ var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
